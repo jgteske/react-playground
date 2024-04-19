973c650bfe0056dc7f13577c7a16861330990684
Johann PC <jteske0104@gmail.com>
2024-04-19 17:32:32 +0200

    chore: add a basic setup for an api

---
+++ my-app/src/app/api/testroute/route.ts
@@ -0,0 +1,10 @@
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+
+  const requestHeaders = new Headers(request.headers);
+
+  return Response.json(
+    { text: "This is a subnode from the api", url: request.url },
+    { headers: requestHeaders, status: 200 }
+  );
+}
