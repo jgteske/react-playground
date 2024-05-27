940d4b520ab67131d00d1c94295ac86cb4b0b7db
Johann <jteske0104@gmail.com>
2024-05-27 23:28:37 +0200

    chore: add basic animation for css with Animate.css for page laoding

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 This projects can be used to test and develope react elements
 
 Additionally it can be used to configure different API endpoints.
+
+## Used Technologies
+
+- React
+- next.js
+- tailwind
+- [Animate.css](https://animate.style/)
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -7,6 +7,11 @@ export default function Home() {
         <h1>This is an example Homepage</h1>
         <br />
         <p>Playground to try different things with react, next and ts.</p>
+        <br></br>
+        {/* Animations to chose from under https://animate.style/ */}
+        <div className="animate__animated animate__fadeInTopLeft animate__slow animate__delay-1s">
+          Animate Text on Page Loading with Animate.css!
+        </div>
       </div>
     </RootLayout>
   );
