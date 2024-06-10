c2398805edf40cb7796e8b47f615ca8734bd3b58
Johann <jteske0104@gmail.com>
2024-06-10 19:10:11 +0200

    chore: add example mui pages

---
+++ my-app/src/app/(hidden)/landingpage/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import LandingPage from '@/components/muiTemplates/landing-page/LandingPage';
+
+const LandingPageExample = () => {
+  return <LandingPage />;
+};
+
+export default LandingPageExample;
---
+++ my-app/src/app/(hidden)/login/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import SignInSide from '@/components/muiTemplates/sign-in-side/SignInSide';
+
+const LoginPageExample = () => {
+  return <SignInSide />;
+};
+
+export default LoginPageExample;
