ffc80b78b04c427f077ad74ad56391e2caab4731
Johann <jteske0104@gmail.com>
2024-06-10 18:43:54 +0200

    chore: add example dashboard page from template

---
+++ my-app/src/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import Dashboard from '@/components/muiTemplates/dashboard/Dashboard';
+
+const DashboardExamplePage = () => {
+  return <Dashboard />;
+};
+
+export default DashboardExamplePage;
