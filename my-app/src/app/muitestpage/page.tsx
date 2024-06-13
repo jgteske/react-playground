2fbad5bb3828fa2b2ecc2d37c63f22c434ed8fea
Johann <jteske0104@gmail.com>
2024-06-13 15:45:02 +0200

    chore: mui theme wrapper to toggle dark and light mode in application

--- my-app/src/app/muitestpage/page.tsx
+++ my-app/src/app/muitestpage/page.tsx
@@ -1,39 +1,44 @@
 'use client';
 
 import RootLayout from '@/components/Layout';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
 
 import { AutocompleteExample } from './components/autocomplete';
 import { AccordionExample } from './components/accordion';
 import { SkeletonExample } from './components/skeleton';
-
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
+import MuiThemeWrapper, {
+  ToggleThemePaletteMode,
+} from '@/components/MuiThemeWrapper';
 
 const MuiTestPage: React.FC = () => {
   return (
-    <RootLayout>
-      <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-        <h1>Mui Testpage</h1>
-        {/* Example autocomplete field */}
-        <div className='py-4'>
-          <AutocompleteExample />
-        </div>
-        {/* Accordion Example */}
-        <div className='py-4'>
-          <AccordionExample />
-        </div>
-        {/* Skeleton Example */}
-        <div className='py-4 w-[300px]'>
-          <SkeletonExample />
-        </div>
-      </ThemeProvider>
-    </RootLayout>
+    <MuiThemeWrapper>
+      <RootLayout>
+        <ToggleThemePaletteMode />
+        <Container
+          sx={{
+            width: '100%',
+            alignItems: 'center',
+            justifyContent: 'left',
+            bgcolor: 'background.default',
+            color: 'text.primary',
+          }}>
+          <h1>Mui Testpage</h1>
+          {/* Example autocomplete field */}
+          <div className='py-4'>
+            <AutocompleteExample />
+          </div>
+          {/* Accordion Example */}
+          <div className='py-4'>
+            <AccordionExample />
+          </div>
+          {/* Skeleton Example */}
+          <div className='py-4 w-[300px]'>
+            <SkeletonExample />
+          </div>
+        </Container>
+      </RootLayout>
+    </MuiThemeWrapper>
   );
 };
 
---
+++ my-app/src/components/MuiThemeWrapper.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import IconButton from '@mui/material/IconButton';
+import CssBaseline from '@mui/material/CssBaseline';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+
+interface MuiThemeWrapperProps {
+  children: React.ReactNode;
+}
+
+const MuiThemeWrapper = ({ children }: MuiThemeWrapperProps) => {
+  const isSystemDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const [themePaletteMode, setThemePaletteMode] = useState(
+    isSystemDarkMode ? 'dark' : 'light'
+  );
+
+  const themePaletteModeContextProvider = React.useMemo(
+    () => ({
+      toggleThemePaletteMode: () => {
+        setThemePaletteMode((prevMode) =>
+          prevMode === 'light' ? 'dark' : 'light'
+        );
+      },
+    }),
+    []
+  );
+
+  const themeProvider = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: themePaletteMode === 'dark' ? 'dark' : 'light',
+        },
+      }),
+    [themePaletteMode]
+  );
+
+  return (
+    <ThemePaletteModeContext.Provider value={themePaletteModeContextProvider}>
+      <ThemeProvider theme={themeProvider}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </ThemePaletteModeContext.Provider>
+  );
+};
+
+const ThemePaletteModeContext = React.createContext({
+  toggleThemePaletteMode: () => {},
+});
+
+export const ToggleThemePaletteMode = () => {
+  const theme = useTheme();
+  const themePaletteModeContext = React.useContext(ThemePaletteModeContext);
+  return (
+    <IconButton
+      sx={{ ml: 1 }}
+      onClick={themePaletteModeContext.toggleThemePaletteMode}
+      color='inherit'>
+      {theme.palette.mode === 'dark' ? (
+        <Brightness7Icon />
+      ) : (
+        <Brightness4Icon />
+      )}
+    </IconButton>
+  );
+};
+
+export default MuiThemeWrapper;
